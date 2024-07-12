f27698313a068ce495d2b7497aca2d451bd75ed1
Mahmudulshawn <mahmudulalamshawn@gmail.com>
2024-07-12 17:50:41 +0600

    Todo list with Context and local storage

--- ContextTodo/src/Contexts/TodoContext.jsx
+++ ContextTodo/src/Contexts/TodoContext.jsx
@@ -1,17 +1,21 @@
-import { useContext, createContext } from "react";
+import { createContext,useContext } from "react";
 
 
 export const TodoContext = createContext({
-    todos: [{
+    todos: [
+        {
         id: 1,
         todo: 'Task 1',
         completed: false,
-    }],
-    addTodo: (todo) => {},
-    updateTodo: (id, todo) => {},
-    deleteTodo: (id) => {},
-    toggleComplete: (id) => {},
+        },
+    ],
+    addTodo: (todo)=>{},
+    deleteTodo: (id)=>{},
+    updateTodo: (id, todo)=>{},
+    toggleComplete: (id)=>{},
 });
+
+
 
 export const useTodo = () => {
     return useContext(TodoContext);
