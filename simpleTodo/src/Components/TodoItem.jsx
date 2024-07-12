6aca03d9b58ef8aa0dfa61d7e2a2628d2cc973d0
Mahmudulshawn <mahmudulalamshawn@gmail.com>
2024-07-12 19:00:46 +0600

    Added a simple todo that has only add and delete functionality while revising context and local storage

---
+++ simpleTodo/src/Components/TodoItem.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+import { useTodo } from '../Contexts/TodoContext';
+
+function TodoItem({todo}) {
+
+    const [todoMsg, setTodoMsg] = useState(todo.todo);
+
+    const {deleteTodo} = useTodo();
+
+  return (
+    <div className='flex border border-black/10 rounded-lg px-3 py-1.5 gap-x-3 shadow-sm shadow-white/50 duration-300  text-white'>
+        <input
+            type="text"
+            className='outline-none w-full bg-transparent rounded-lg'
+            value={todoMsg}
+            onChange={(e) => setTodoMsg(e.target.value)}
+            readOnly
+        />
+        <button
+            className="inline-flex w-8 h-8 rounded-lg text-sm border border-black/10 justify-center items-center shrink-0"
+            onClick={() => deleteTodo(todo.id)}
+        >
+            Delete
+        </button>
+    </div>
+  )
+}
+
+export default TodoItem
