8d843a7258a71183cbd44fa421bc70e32bea27f6
Mahmudulshawn <mahmudulalamshawn@gmail.com>
2024-07-26 17:55:27 +0600

    finished with the website

--- BlogWebsite/src/Components/Container/Container.jsx
+++ BlogWebsite/src/Components/Container/Container.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Container({children}) {
   return (
-    <div w-full max-w-7xl mx-auto pax-4  >{children}</div> 
+    <div className="w-full max-w-7xl mx-auto px-4">{children}</div> 
   )
 }
 
--- BlogWebsite/src/Components/index.js
+++ BlogWebsite/src/Components/index.js
@@ -3,11 +3,32 @@
  import Container from './Container/Container'
  import Logo from './Logo'
  import LogoutBtn from './Header/LogoutBtn'
+ import RTE from './RTE'
+ import Signup from '../pages/Signup'
+ import Login from '../pages/Login'
+//  import Post from '../pages/Post'
+ import PostForm from './post-form/PostForm'
+ import PostCard from './PostCard'
+ import AuthLayout from './AuthLayout'
+ import Button from './Button'
+ import Input from './Input'
+ import Select from './Select'
+
 
  export {
-    Container,
-    Header,
-    Footer,
-    Logo,
-    LogoutBtn,
+   RTE,
+   Container,
+   Header,
+   Footer,
+   Logo,
+   LogoutBtn,
+   Signup,
+   Login,
+  //  Post,
+   PostForm,
+   PostCard,
+   AuthLayout,
+   Button,
+   Input,
+   Select,
  }
